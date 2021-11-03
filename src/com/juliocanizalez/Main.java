1d669e0279a353b4559626b7702c1e8a86585892
Julio Canizalez <juliocanizalez@outlook.com>
2021-11-03 11:00:20 -0600

    :hammer: Add TreeSet ouputs

--- src/com/juliocanizalez/Main.java
+++ src/com/juliocanizalez/Main.java
@@ -5,6 +5,8 @@ public class Main {
   public static void main(String[] args) {
     HashSetExample hSet = new HashSetExample();
     LinkedHashSetExample lhSet = new LinkedHashSetExample();
+    TreeSetExample tSet = new TreeSetExample();
+
     // HashSet Example
     hSet.addValues();
     hSet.removeValue(2);
@@ -14,5 +16,10 @@ public class Main {
     lhSet.addValues();
     lhSet.updateValue(1, "LinkedHashSet");
     lhSet.removeValue(2);
+
+    // TreeSet Example
+    tSet.addValues();
+    tSet.removeValue(2);
+    tSet.updateValue(1, "TreeSetExample");
   }
 }
