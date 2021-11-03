a1c78ca6e69d4255ba53884305ecf227074a93c2
Julio Canizalez <juliocanizalez@outlook.com>
2021-11-03 10:30:19 -0600

    :hammer: Add LinkedHashSet outputs

--- src/com/juliocanizalez/Main.java
+++ src/com/juliocanizalez/Main.java
@@ -4,9 +4,15 @@ public class Main {
 
   public static void main(String[] args) {
     HashSetExample hSet = new HashSetExample();
-
+    LinkedHashSetExample lhSet = new LinkedHashSetExample();
+    // HashSet Example
     hSet.addValues();
     hSet.removeValue(2);
     hSet.updateValue(1, "HashSetExample");
+
+    // LinkedHashSet Example
+    lhSet.addValues();
+    lhSet.updateValue(1, "LinkedHashSet");
+    lhSet.removeValue(2);
   }
 }
