e62640e7b8f2ef23487e2907bec754998901cc84
Julio Canizalez <juliocanizalez@outlook.com>
2021-11-03 10:09:35 -0600

    :hammer: Display hashset example

--- src/com/juliocanizalez/Main.java
+++ src/com/juliocanizalez/Main.java
@@ -2,7 +2,11 @@ package com.juliocanizalez;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+  public static void main(String[] args) {
+    HashSetExample hSet = new HashSetExample();
+
+    hSet.addValues();
+    hSet.removeValue(2);
+    hSet.updateValue(1, "HashSetExample");
+  }
 }
